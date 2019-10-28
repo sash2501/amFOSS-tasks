ac6614d489980a8e3c91661161486f5cb535ba0b
sash2501 <sashmitaraghav2505@gmail.com>
2019-10-28 18:23:50 +0530

    renamed files

--- task-11/Pong Game.md
+++
@@ -1,4 +0,0 @@
-The Pong repository:
-https://github.com/sash2501/Pong-Game
----
-You can play the game at: https://sash2501.github.io/Pong-Game/
--- task-8/captcha.py
+++
@@ -1,9 +0,0 @@
-import pytesseract
-from PIL import Image
-img=Image.open('picmult.png')
-text=pytesseract .image_to_string(img)
-print('The expression written in image is :  ')
-print(text)
-c = eval(text)
-print('The solution of the expression is: ')
-print(c)
